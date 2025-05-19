e10062ba90363c9f3943cd38af205475300322e1
Dntaa <diwadinatasimarmata@gmail.com>
2025-05-19 15:44:09 +0700

    2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,9 +35,9 @@
                         <a href="/transaksi" class="block px-4 py-2 rounded hover:bg-indigo-600 transition">🔄 Transaksi</a>
                     </li>
                     <li>
-                        <form action="{{ route('logout') }}" method="POST" class="flex gap-3">
+                        <form action="{{ route('logout') }}" method="POST" class="">
                         @csrf
-                            <button type="submit" class="block px-4 py-2 rounded hover:bg-indigo-600 transition gap-2">
+                            <button type="submit" class="px-4 py-2 rounded hover:bg-indigo-600 transition flex gap-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" /></svg>Logout
                             </button>
